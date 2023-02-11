af6e5eab8ce7f1eb39cf17b71c0dab3302c915df
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-02-11 17:43:10 -0300

    <feat> Find one payment without cvv done

--- finance/src/controllers/payment.controller.js
+++ finance/src/controllers/payment.controller.js
@@ -7,16 +7,28 @@ const findAll = async (_req, res) => {
   return res.status(HTTPStatus.OK).json(response);
 };
 
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const response = await Payments.findByPk(id);
+  const { cvv:_, ...responseWithoutCVV } = response.dataValues;
+
+  return res.status(HTTPStatus.OK).json(responseWithoutCVV);
+}
+
 const create = async (req, res) => {
   const payload = req.body;
   const valid = validations.payloadValidation(payload);
   if (valid) return res.status(valid.code).send(valid.message);
 
   const response = await Payments.create(payload);
-  return res.status(HTTPStatus.CREATED).json(response);
+  return res
+    .status(HTTPStatus.CREATED)
+    .header({location: `/api/payments/${response.id}`})
+    .json(response);
 }
 
 module.exports = {
   findAll,
+  findById,
   create,
 }
--- finance/src/routes/payment.routes.js
+++ finance/src/routes/payment.routes.js
@@ -4,6 +4,7 @@ const paymentController = require('../controllers/payment.controller');
 const route = Router();
 
 route.get('/', paymentController.findAll);
+route.get('/:id', paymentController.findById);
 route.post('/', paymentController.create);
 
 module.exports = route;
