bb7feaba8252eee755bbe97da5bb7894ae79a5a5
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-28 06:10:53 -0300

    <fix> Removing authentication of each microservice and keeping just on gateway

--- finance/src/app.js
+++ finance/src/app.js
@@ -1,8 +1,6 @@
 require('express-async-errors');
-require('./middlewares/auth.middleware');
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
 const paymentRoutes = require('./routes/payment.routes');
 const errorMiddleware = require('./middlewares/error.middleware');
 
@@ -10,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+
 app.get('/health-check', (_req, res) => res.status(200).send('Connection OK'));
 app.use('/api/payments', paymentRoutes);
 app.use(errorMiddleware);
--- finance/src/routes/payment.routes.js
+++ finance/src/routes/payment.routes.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
-const passport = require('passport');
 const paymentController = require('../controllers/payments.controller');
 const statusMiddleware = require('../middlewares/status.middleware');
 
 const route = Router();
 
-route.use(passport.authenticate('bearer', { session: false }));
 route.get('/', paymentController.findAll);
 route.get('/:id', paymentController.findById);
 route.post('/', paymentController.create);
