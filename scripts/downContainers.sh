e87c1ef9a30fe27a25e00f820c6e9496689fadec
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-01 17:36:16 -0300

    <setup> Updating all submodules

---
+++ scripts/downContainers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Down product container
+cd ../trooper-ms-product && docker-compose down
+
+# Down account container
+cd ../trooper-ms-account && docker-compose down
+
+# Down order container
+cd ../trooper-ms-order && docker-compose down
+
+# Down payment container
+cd ../trooper-ms-payment && docker-compose down
+
+# Down gateway container
+cd ../trooper-ms-gateway && docker-compose down
+
+# Down frontend container
+cd ../trooper-frontend && docker-compose down
+
+# Delete network
+docker network rm trooper-network
---
+++ scripts/upContainers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create network
+docker network create trooper-network
+
+# Up product container
+cd ../trooper-ms-product && docker-compose up -d --build
+
+# Up account container
+cd ../trooper-ms-account && docker-compose up -d --build
+
+# Up order container
+cd ../trooper-ms-order && docker-compose up -d --build
+
+# Up payment container
+cd ../trooper-ms-payment && docker-compose up -d --build
+
+# Up gateway container
+cd ../trooper-ms-gateway && docker-compose up -d --build
+
+# Up frontend container
+cd ../trooper-frontend && docker-compose up -d --build
