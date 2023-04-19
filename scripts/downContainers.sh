5f351b7cee1ba3dae5c6d1836ec0badc2a73b995
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-19 00:11:47 -0300

    <setup> Updating frontend and account submodules to latest commit

--- scripts/downContainers.sh
+++ scripts/downContainers.sh
@@ -18,5 +18,8 @@ cd ../trooper-ms-gateway && docker-compose down
 # Down frontend container
 cd ../trooper-frontend && docker-compose down
 
+# Down rabbit container
+cd ../ && docker-compose down
+
 # Delete network
 docker network rm trooper-network
